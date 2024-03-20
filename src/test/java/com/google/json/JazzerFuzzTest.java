fe015fb0a4fb337ffa2171fbcc50e7ff5346a11d
Matteo Pernarella <matteo.pernarella@sourcesense.com>
2024-03-20 12:57:05 +0100

    added some more tests

--- src/test/java/com/google/json/JazzerFuzzTest.java
+++ src/test/java/com/google/json/JazzerFuzzTest.java
@@ -2,6 +2,8 @@ package com.google.json;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.junit.FuzzTest;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueMedium;
 
 public class JazzerFuzzTest {
 
@@ -9,6 +11,7 @@ public class JazzerFuzzTest {
     public void testSanitizerWithFuzzyInputs(FuzzedDataProvider data) {
         try {
             JsonSanitizer.sanitize(data.consumeRemainingAsString(), 10);
+            JsonSanitizer.sanitize(data.consumeString(4096));
         } catch (ArrayIndexOutOfBoundsException ignored) {
 
         }
@@ -24,22 +27,44 @@ public class JazzerFuzzTest {
     }
 
     @FuzzTest
-    public void testMinifyWithFuzzyInputs(FuzzedDataProvider data) {
-        try {
-            EvalMinifier.minify(data.consumeRemainingAsString(), 10);
-        } catch (ArrayIndexOutOfBoundsException ignored) {
+    public void testForbidden(FuzzedDataProvider data) {
+    String input = data.consumeRemainingAsString();
+    String validJson;
+    try {
+      validJson = JsonSanitizer.sanitize(input, 10);
+    } catch (Exception e) {
+      return;
+    }
 
-        }
+    // Check for forbidden substrings. As these would enable Cross-Site Scripting, treat every
+    // finding as a high severity vulnerability.
+    assert !validJson.contains("</script")
+        : new FuzzerSecurityIssueHigh("Output contains </script");
+    assert !validJson.contains("]]>") : new FuzzerSecurityIssueHigh("Output contains ]]>");
+
+    // Check for more forbidden substrings. As these would not directly enable Cross-Site Scripting
+    // in general, but may impact script execution on the embedding page, treat each finding as a
+    // medium severity vulnerability.
+    assert !validJson.contains("<script")
+        : new FuzzerSecurityIssueMedium("Output contains <script");
+    assert !validJson.contains("<!--") : new FuzzerSecurityIssueMedium("Output contains <!--");
     }
 
     @FuzzTest
-    public void testMinifyWithFuzzyDepth(FuzzedDataProvider data) {
-        try {
-            EvalMinifier.minify(data.consumeRemainingAsString(), data.consumeInt());
-        } catch (ArrayIndexOutOfBoundsException ignored) {
-
-        }
+    public void testDoubleSanitize(FuzzedDataProvider data) {
+    String input = data.consumeRemainingAsString();
+    String validJson;
+    try {
+      validJson = JsonSanitizer.sanitize(input, 10);
+    } catch (Exception e) {
+      return;
     }
+
+    // Ensure that sanitizing twice does not give different output (idempotence). Since failure to
+    // be idempotent is not a security issue in itself, fail with a regular AssertionError.
+    assert JsonSanitizer.sanitize(validJson).equals(validJson) : "Not idempotent";
+  }
+
 
 
 }
